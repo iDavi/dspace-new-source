f5b3764c07e604c103f44ad567f3d9c714498212
iDavi <odavi20527@gmail.com>
2025-05-09 00:02:35 -0300

    now not any music  start playing from nothing in home but only pipiooopopo wwwowo

--- src/app/d-music-radio/d-music-radio.component.ts
+++ src/app/d-music-radio/d-music-radio.component.ts
@@ -1,37 +1,58 @@
-import { NgForOf } from '@angular/common';
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { NgForOf } from "@angular/common";
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-d-music-radio',
+  selector: "app-d-music-radio",
   standalone: true,
   imports: [NgForOf],
-  templateUrl: './d-music-radio.component.html',
-  styleUrls: ['./d-music-radio.component.css']
+  templateUrl: "./d-music-radio.component.html",
+  styleUrls: ["./d-music-radio.component.css"],
 })
 export class DMusicRadioComponent implements OnInit, AfterViewInit {
-  tracks: string[] = ['round-table-puzzle', 'the-ghost-of-you-mcr', 'u-x-me-laurales', 'pastel-ghost', 'angel-of-darkness-nightcore', 'pipopipiooii', 'between-the-cureee','people-i-dont-like-upsahl','helena-so-long-and-goodnight-mcr','artist-bo-burnham','you-lose-margarina-bay','carlosito-jalapeno-keller-synth-muito-foda', 's3rl-s3rl-s3rl'];
-  currentTrack: string = '';
-  musicUrl: string = '';
+  tracks: string[] = [
+    "round-table-puzzle",
+    "the-ghost-of-you-mcr",
+    "u-x-me-laurales",
+    "pastel-ghost",
+    "angel-of-darkness-nightcore",
+    "pipopipiooii",
+    "between-the-cureee",
+    "people-i-dont-like-upsahl",
+    "helena-so-long-and-goodnight-mcr",
+    "artist-bo-burnham",
+    "you-lose-margarina-bay",
+    "carlosito-jalapeno-keller-synth-muito-foda",
+    "s3rl-s3rl-s3rl",
+  ];
+  currentTrack: string = "";
+  musicUrl: string = "";
   isPlaying: boolean = false;
   private firstClickHandler = this.handleFirstClick.bind(this);
-  @ViewChild('audioPlayer') audioPlayer!: ElementRef;
+  @ViewChild("audioPlayer") audioPlayer!: ElementRef;
 
   ngOnInit() {
-    this.randomizeMusic();
+    this.currentTrack = this.tracks[5];
+    this.musicUrl = `music/${this.currentTrack}.mp3`;
   }
-  
+
   ngAfterViewInit() {
-    document.addEventListener('click', this.firstClickHandler);
+    document.addEventListener("click", this.firstClickHandler);
   }
-  
+
   handleFirstClick() {
-    if(this.audioPlayer && this.audioPlayer.nativeElement) {
+    if (this.audioPlayer && this.audioPlayer.nativeElement) {
       this.audioPlayer.nativeElement.play();
       this.isPlaying = true;
     }
-    document.removeEventListener('click', this.firstClickHandler);
+    document.removeEventListener("click", this.firstClickHandler);
   }
-  
+
   randomizeMusic() {
     const randomIndex = Math.floor(Math.random() * this.tracks.length);
     this.currentTrack = this.tracks[randomIndex];
@@ -41,15 +62,15 @@ export class DMusicRadioComponent implements OnInit, AfterViewInit {
   selectTrack(track: string) {
     this.currentTrack = track;
     this.musicUrl = `music/${track}.mp3`;
-    if(this.audioPlayer && this.audioPlayer.nativeElement) {
+    if (this.audioPlayer && this.audioPlayer.nativeElement) {
       this.audioPlayer.nativeElement.play();
       this.isPlaying = true;
     }
   }
-  
+
   togglePlayback() {
     const audio = this.audioPlayer.nativeElement;
-    if(audio.paused){
+    if (audio.paused) {
       audio.play();
       this.isPlaying = true;
     } else {
